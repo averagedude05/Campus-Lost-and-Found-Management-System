ba9551a3352dbafbb83ce2c30bc7dc271874ac74
averagedude05 <adibkhan642@gmail.com>
2026-08-14 17:05:06 +0600

    report found item html+css(initial push)

---
+++ View/Report Found Item.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<head>
+    <title>Campus Lost and Found</title>
+    <link rel="stylesheet" href="found_item.css">
+</head>
+
+<body>
+    <h2>Item Information</h2>
+    <div class="container">
+        <div class="image">
+            <img src="found_item.jpg" alt="Found Item">
+        </div>
+        <div class="form">
+            <table>
+                <tr>
+                    <td>
+                        <label for="itemnametxt" class="label">Item Name</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="itemnametxt" id="itemnametxt" class="input">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="itemcatagory" class="label">Category</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <select name="itemcatagory" id="itemcatagory" class="input">
+                            <option selected disabled hidden></option>
+                            <option value="Id">ID Card</option>
+                            <option value="electronics">Electronics</option>
+                            <option value="bag">Bag</option>
+                            <option value="others">Others</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="founddate" class="label">Date</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="date" name="founddate" id="founddate" class="input">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="foundlocation" class="label">Location</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" name="foundlocation" id="foundlocation" class="input">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="description" class="label">Description</label>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <textarea placeholder="Enter description of the item"
+                                  id="description"
+                                  name="description"
+                                  class="input"></textarea>
+                    </td>
+                </tr>
+            </table>
+            <button type="submit" class="submit-btn">Submit</button>
+            <button type="reset" class="reset-btn">Reset</button>
+        </div>
+    </div>
+</body>
