aa9f9ab7ab51d20745e3c86895a72f1b25ea2d58
averagedude05 <adibkhan642@gmail.com>
2026-09-14 01:55:04 +0600

    minor change

--- View/navbar.php
+++ View/navbar.php
@@ -7,7 +7,7 @@
 
     <li><a href="../Controller/Report Found Item Controller.php">Report New Item</a></li>
     <li> <a href="../Controller/logoutController.php" onclick="return confirm('Are you sure you want to logout?');">Logout</a></li>
-    <li><a href="#about">My Profile</a></li>
+    
     
 </ul>
 
